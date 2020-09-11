fc2345aae2976fd2d7d76c4836a79cdb79b0d4cb
Justin Rodriguez <rmjustin02@gmail.com>
2020-09-11 13:14:58 +0200

    responsive

--- private/views/default.php
+++ private/views/default.php
@@ -5,20 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Mike Yang, Arjan van Batenburg, Cecile Enzler, Justin Rodriguez Montoya">
 
+    <script src="https://kit.fontawesome.com/7023acb312.js" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="<?php echo site_url('/css/main.css') ?>">
+    <link rel="stylesheet" href="<?php echo site_url('/css/nav.css') ?>">
     <?php echo $this->section('stylesheets'); ?>
 
     <title> <?php echo $this->section('title'); ?> • CreativeBabe</title>
 </head>
 <body>
     <nav>
-        <ul>
+        <img src="<?php echo site_url('/img/logo.png') ?>" alt="logo">
+
+        <ul class="desktop">
             <li><a href="">Home</a></li>
             <li><a href="">Over ons</a></li>
             <li><a href="">Temp</a></li>
         </ul>
-        <img src="<?php echo site_url('/img/logo.png') ?>" alt="logo">
+
+        <div class="mobile">
+            <button>Test <span class="fas fa-caret-down dropdown-open"></span></button>
+        </div>
     </nav>
+
 
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
