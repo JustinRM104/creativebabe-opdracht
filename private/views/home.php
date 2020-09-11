7a731a4efe4edf5e27e506ca9cec9dddf94ca06d
Justin Rodriguez <rmjustin02@gmail.com>
2020-09-11 16:24:36 +0200

    home

--- private/views/home.php
+++ private/views/home.php
@@ -2,8 +2,21 @@
 
 <?php $this->start('title'); echo "Home"; $this->stop(); ?>
 
+<?php $this->start('stylesheets'); ?>
+
+<!-- Laad hier de stylesheets in. -->
+<!--      <link rel="stylesheet" href="<?php echo site_url('/css/nav.css') ?>">         -->
+<!-- copy paste dit hierboven gwn en verander naar naar goede bestand. -->
+
+
+<?php $this->stop(); ?>
+
 <?php $this->start('cont'); ?>
 
+
+<!-- Begin hier met werken aan de home pagina -->
 <source src="<?php echo site_url('/videos/promo.mp4') ?>" type="video/mp4">
+
+
 
 <?php $this->stop(); ?>
