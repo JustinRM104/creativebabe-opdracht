77e591edc05e3fc17164259129ae88cf8005c0fe
Justin Rodriguez <rmjustin02@gmail.com>
2020-09-17 11:18:35 +0200

    update

--- private/views/home.php
+++ private/views/home.php
@@ -16,18 +16,12 @@
     </video>
 </div>
 
-<div class="about" id="over">
-    <div class="wrapper-about">
-      <div class="intro-image">
-        <img src="https://www.vorkaccountants.nl/wp-content/uploads/2018/01/placeholder-1024x1024.png" alt="Profielfoto">
-      </div>
-      <div class="intro-txt">
+<div class="about">
+  <img src="https://www.vorkaccountants.nl/wp-content/uploads/2018/01/placeholder-1024x1024.png" alt="foto">
+  <div class="intro-txt">
         <h2>Random Titel</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-      </div>
-    </div>
+  </div>
 </div>
 
 <div class="cta" id="cta">
@@ -36,6 +30,35 @@
     <a href="#" class="cta-btn">Neem nu contact op!</a>
 </div>
 
-
+<div class="workFeed">
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+  <div>
+    <h3>Temp</h3>
+    <img src="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+  </div>
+</div>
 
 <?php $this->stop(); ?>
